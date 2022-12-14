4a1d0469cbcec1c38995d95c1e8da09928f2e3da
maminiriana <maminiriana@gmail.com>
2022-12-14 18:06:50 +0300

    Add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	ini_set("register_globals","off");
+	ini_set("display_errors","off");
+	session_start();
+	$_SESSION["session"]="";
+	
+	$requete->nom="";
+
+	$requete->erreurs=array();
+	include"templates/reserv.php";
+	exit(0);	
+	
+?>
